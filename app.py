82ac6f985ed0f789da66b9467115851931518eca
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-28 00:07:47 +0500

    logical op with assingment

--- app.py
+++ app.py
@@ -13,3 +13,40 @@
 # # 10 + 10
 # num2+ 1
 # print(num2)
+# num1: int = 2
+# num2: int = 2
+# print(num1 == num2)
+# student1: str = "Saeed"
+# student2: str = "Saeed"
+
+# print(student1 == student2)
+
+# boolean1: bool = True
+# boolean2: bool = True
+# print(boolean1 == boolean2)
+
+# num1 = True
+# num2 =  False
+# print(num1 != num2)
+# print(10 < 20 > 15 == 15)
+
+
+# name1 = "Saeed"
+# nam2 = "Ali"
+#aur both conditions should be true >> true wrna false
+# print( 6 > 4 and 10 < 10)
+
+#or 2 conditions mai se 1 bh agr True >> True
+# print( 4 != 7 and 10 != 10) #true
+
+# light: str = "Yes"
+# ups: str = "Off"
+# result = light == "Yes" and ups == "Off"
+
+# print(result)
+
+juice: str = "available"  
+water : str = "availble"
+
+result = water != juice 
+print(result)

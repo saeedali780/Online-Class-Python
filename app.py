c6495a6fc67d8c219393746f49ee6715205488b3
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-27 22:23:36 +0500

    comparison operators started

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 # print("Hello world")
 
 #Arthmetic op
-age: int = 18
-ageIncreased: int = 3
-ageResult: int = age - ageIncreased
-print(age)
+# age: int = 18
+# ageIncreased: int = 3
+# ageResult: int = age - ageIncreased
+# print(age)
 
 # age is 17
+
+# num1: int = 10
+# num2: int = 40
+# # 10 + 10
+# num2+ 1
+# print(num2)
